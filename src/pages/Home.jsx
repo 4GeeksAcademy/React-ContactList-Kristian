ca60f23652802f6191e4bbaf86ce25bdc48146a5
Kristian Pacheco <kristianpachecar18@gmail.com>
2025-07-18 00:04:59 +0000

    Added functions to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,75 @@
 import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Home = () => {
+	const navigate = useNavigate();
+	
+	const [list, setList] = useState([]);
+	const [agenda, setAgenda] = useState("");
+	const [isAgendaCreated, setIsAgendaCreated] = useState(false);
+	const { store, dispatch } = useGlobalReducer()
 
-  const {store, dispatch} =useGlobalReducer()
+	const handleCreateAgenda = () => {
+		fetch(`https://playground.4geeks.com/contact/agendas/${agenda}`, {
+			method: "POST",
+			body: JSON.stringify([]),
+			headers: { "Content-Type": "application/json" }
+		})
+			.then(res => {
+				if (res.ok) {
+					setIsAgendaCreated(true);
+					getContacts();
+				} else {
+					alert("This agenda is already created");
+					setIsAgendaCreated(true);
+					getContacts();
+				}
+			})
+			.catch(err => console.error("There was an error creating agenda:", err));
+	};
+
+	const handleDelete = (contactId) => {
+		fetch(`https://playground.4geeks.com/contact/agendas/${agenda}/${contactId}`, {
+			method: "DELETE"
+		})
+			.then(() => getContacts())
+			.catch(err => console.error("There was an error deleting contact:", err));
+	};
+
+	const getContacts = () => {
+		fetch(`https://playground.4geeks.com/contact/agendas/${agenda}/contacts`)
+			.then(res => res.json())
+			.then(data => {
+				if (Array.isArray(data)) {
+					setList(data);
+				} else if (Array.isArray(data.contacts)) {
+					setList(data.contacts);
+				} else {
+					console.error("Unexpected data structure:", data);
+					setList([]);
+				}
+			})
+			.catch(err => console.error("There was an error getting contacts:", err));
+	};
+
+	const createContact = (taskLabel) => {
+		fetch(`https://playground.4geeks.com/todo/todos/${username}`, {
+			method: "POST",
+			body: JSON.stringify({ label: taskLabel, is_done: false }),
+			headers: { "Content-Type": "application/json" }
+		})
+			.then(() => {
+				setTask("");
+				getTasks();
+			})
+			.catch(err => console.error("There was an error creating task:", err));
+	};
 
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
+
 		</div>
 	);
 }; 
