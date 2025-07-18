18c8bd4efeff6ff924652b9427ccf83b70793567
Kristian Pacheco <kristianpachecar18@gmail.com>
2025-07-18 04:13:31 +0000

    Added some style to button in contact card

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -2,7 +2,7 @@ import { FaEdit, FaTrash } from "react-icons/fa";
 
 export const ContactCard = ({ contact, onEdit, onDelete }) => {
     return (
-        <div className="card mb-3" style={{ width: "700px" }}>
+        <div className="card mb-3 position-relative" style={{ width: "700px" }}>
             <div className="row g-0">
                 <div className="col-md-4 d-flex align-items-center justify-content-center">
                     <img
@@ -19,12 +19,12 @@ export const ContactCard = ({ contact, onEdit, onDelete }) => {
                         <p className="card-text mb-1"><span className="m-1"><i class="fa-solid fa-location-dot"></i></span> {contact.address}</p>
                         <p className="card-text mb-1"><span className="m-1"><i class="fa-solid fa-phone"></i></span> {contact.phone}</p>
                         <p className="card-text mb-1"><span className="m-1"><i class="fa-solid fa-envelope"></i></span> {contact.email}</p>
-                        <div className="mt-2">
-                            <button className="btn btn-sm btn-outline-primary me-2" onClick={() => onEdit(contact.id)}>
-                                <FaEdit />
+                        <div className="position-absolute top-0 end-0 p-2">
+                            <button className="btn btn-sm edit-button me-1" onClick={() => onEdit(contact.id)}>
+                                <i class="fa-solid fa-pen-to-square"></i>
                             </button>
-                            <button className="btn btn-sm btn-outline-danger" onClick={() => onDelete(contact.id)}>
-                                <FaTrash />
+                            <button className="btn btn-sm del-button" onClick={() => onDelete(contact.id)}>
+                                <i class="fa-solid fa-trash"></i>
                             </button>
                         </div>
                     </div>
